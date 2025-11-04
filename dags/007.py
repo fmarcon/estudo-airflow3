d3cbd562320bafb0d17c2b29fe3c8c757ff2da1a
Fabiano Marcon <fmarcon@ucs.br>
2025-11-04 18:53:05 -0300

    feat(dag): ✨ Adiciona nova DAG de execução com lógica de ramificação
    * Implementa a DAG `execucao_dag` com tarefas de início, verificação e finalização
    * Adiciona lógica de ramificação para diferentes valores
    * Conecta dependências de forma explícita e legível

---
+++ dags/007.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+
+from airflow.decorators import dag, task
+from click import confirm
+
+
+@dag(
+    dag_id="dag_007",
+    start_date=None,
+    schedule="* * * * *",
+    catchup=False,
+    tags=["2025"],
+)
+def execucao_dag():
+    """
+    """
+
+    @task
+    def inicio():
+        print("Executando início.")
+
+    @task
+    def retorna_valor():
+        return 11
+
+    @task.branch
+    def verifica_valor(valor):
+        if valor == 1:
+            return "valor_igual_1"
+        return "valor_diferente_1"
+    
+    @task
+    def valor_igual_1():
+        print("Valor é igual a 1.")
+
+    @task
+    def valor_diferente_1():
+        print("Valor é diferente de 1.")
+
+    @task.branch
+    def executar_outra_escolha(valor):
+        if valor == 2:
+            return "valor_igual_2"
+        return "valor_diferente_de_2"
+
+    @task
+    def valor_igual_2():
+        print("Valor é igual a 2.")
+    
+    @task
+    def valor_diferente_de_2():
+        print("Valor é diferente de 2.")
+
+
+    @task(trigger_rule="one_success")
+    def fim():
+        print("Executando fim.")
+    
+    # Instancia tasks
+    inicio_task = inicio()
+    valor = retorna_valor()
+    verifica = verifica_valor(valor)
+    valor_igual = valor_igual_1()
+    valor_diferente = valor_diferente_1()
+    valor_igual_2 = valor_igual_2()
+    valor_diferente_de_2 = valor_diferente_de_2()
+    executar_outra_escolha = executar_outra_escolha(valor)
+    fim_task = fim()
+
+    # Conectar dependências de forma explícita e legível
+    inicio_task >> valor
+    valor >> verifica
+
+    # Branch 1
+    verifica >> valor_igual
+    verifica >> valor_diferente
+
+    valor_diferente >> executar_outra_escolha
+    executar_outra_escolha >> [valor_igual_2, valor_diferente_de_2]
+
+    # Todos os caminhos convergem para o fim (fim tem trigger_rule="one_success")
+    [valor_igual, valor_igual_2, valor_diferente_de_2] >> fim_task
+
+execucao_dag = execucao_dag()
